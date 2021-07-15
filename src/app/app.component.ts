4d853f9b5db22977e75175b24994be24427276f4
Shayan <sayansen2507@gmail.com>
2021-07-15 12:01:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task1';
+  op1: number = 0;
+  op2: number = 0;
+  answer = 0;
+  onSave() {
+    this.answer = this.op1 * this.op2;
+  }
 }
